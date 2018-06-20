f45d7a23474fe7e92fd4d1ac970ee2094edd087b
realnuno <inbeom918@gmail.com>
2018-06-19 22:30:20 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,197 @@
+var input;
+var cursor;
+var hiddenInput;
+var content = [];
+var lastContent = "", targetContent = "";
+var inputLock = false;
+var autoWriteTimer;
+
+var isMobile, isIE;
+
+window.onload = function() {
+
+    isMobile = navigator && navigator.platform && navigator.platform.match(/^(iPad|iPod|iPhone)$/);
+
+    isIE = (navigator.appName == 'Microsoft Internet Explorer');
+
+    input = document.getElementById('input');
+
+    hiddenInput = document.getElementById('hiddenInput');
+    hiddenInput.focus();
+
+    cursor = document.createElement('cursor');
+    cursor.setAttribute('class', 'blink');
+    cursor.innerHTML = "|";
+
+    if (!isMobile && !isIE) input.appendChild(cursor);
+
+    function refresh() {
+
+        inputLock = true;
+
+        if (targetContent.length - lastContent.length == 0) return;
+
+        var v = targetContent.substring(0, lastContent.length + 1);
+
+        content = [];
+
+        var blinkPadding = false;
+
+        for (var i = 0; i < v.length; i++) {
+            var l = v.charAt(i);
+
+            var d = document.createElement('div');
+            d.setAttribute('class', 'letterContainer');
+
+            var d2 = document.createElement('div');
+
+            var animClass = (i % 2 == 0) ? 'letterAnimTop' : 'letterAnimBottom';
+
+            var letterClass = (lastContent.charAt(i) == l) ? 'letterStatic' : animClass;
+
+            if (letterClass != 'letterStatic') blinkPadding = true;
+
+            d2.setAttribute('class', letterClass);
+
+            d.appendChild(d2);
+
+            d2.innerHTML = l;
+            content.push(d);
+        }
+
+        input.innerHTML = '';
+
+        for (var i = 0; i < content.length; i++) {
+            input.appendChild(content[i]);
+        }
+
+        cursor.style.paddingLeft = (blinkPadding) ? '22px' : '0';
+
+        if (!isMobile && !isIE) input.appendChild(cursor);
+
+        if (targetContent.length - lastContent.length > 1) setTimeout(refresh, 150);
+        else inputLock = false;
+
+        lastContent = v;
+    }
+
+    if (document.addEventListener) {
+
+        document.addEventListener('touchstart', function(e) {
+            clearInterval(autoWriteTimer);
+            targetContent = lastContent;
+        }, false);
+
+        document.addEventListener('click', function(e) {
+            clearInterval(autoWriteTimer);
+            targetContent = lastContent;
+            hiddenInput.focus();
+        }, false);
+
+        if (!isIE) {
+            // Input event is buggy on IE, so don't bother
+            // (https://msdn.microsoft.com/en-us/library/gg592978(v=vs.85).aspx#feedback)
+            // We will use a timer instead (below)
+            hiddenInput.addEventListener('input', function(e) {
+                e.preventDefault();
+                targetContent = hiddenInput.value;
+                if (!inputLock) refresh();
+
+            }, false);
+        } else {
+            setInterval(function() {
+                targetContent = hiddenInput.value;
+
+                if (targetContent != lastContent && !inputLock) refresh();
+            }, 100);
+        }
+
+    }
+
+    hiddenInput.value = "";
+
+    autoWriteTimer = setTimeout(function() {
+        if (lastContent != "") return;
+        targetContent = "search here...";
+        refresh();
+    }, 2000);
+}
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function() {
+	$('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+
+		fixedContentPos: false
+	});
+    
+    
+    $('.popup-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			titleSrc: function(item) {
+				return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+			}
+		}
+	});
+    
+    
+    
+    
+    
+    
+
+   
+    
+    $("input").keydown(function(e) {
+        e.stopPropagation();
+        if (e.keyCode === 13) {
+            e.preventDefault();
+            $('html, body').animate({scrollTop: $('.js--results').offset().top}, 1000);
+            console.log(40);
+        };
+    });
+    
+    
+    
+    
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    
+});
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
