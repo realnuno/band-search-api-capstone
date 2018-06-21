b4c72d4a18540006c0393cf850546fcfa69adb4d
realnuno <inbeom918@gmail.com>
2018-06-21 18:01:20 -0400

    added-css

--- index.js
+++ index.js
@@ -1,181 +1,173 @@
-
-
-
 $(document).ready(function () {
-	
-	
-	
-	
-	let userSearch;
-	
-	 $("input").keydown(function (e) {
+
+
+
+
+    let userSearch;
+
+    $("input").keydown(function (e) {
         e.stopPropagation();
         if (e.keyCode === 13) {
             e.preventDefault();
-			
-			userSearch = $("input").val();
-			$("input").val("");
-			
-			getPhotoResult(userSearch, displayPhotoData);
-			
-			getYoutubeResult(userSearch, displayYoutubeData)
-			
-			getConcertInfo(userSearch, displayConcertInfo);
-			
-			
+
+            userSearch = $("input").val();
+            $("input").val("");
+
+            getPhotoResult(userSearch, displayPhotoData);
+
+            getYoutubeResult(userSearch, displayYoutubeData)
+
+            getConcertInfo(userSearch, displayConcertInfo);
+
+
             $('html, body').animate({
                 scrollTop: $('.js--results').offset().top
             }, 1000);
         };
     });
-	
-	
-    
-    
-    
-/******************Youtube Data*************************/
-	
-	
-    function getYoutubeResult(userInput, callback){
-        
+
+
+
+
+
+    /******************Youtube Data*************************/
+
+
+    function getYoutubeResult(userInput, callback) {
+
         const settings = {
-            url : "https://www.googleapis.com/youtube/v3/search",
-            data : {
-				part: "snippet", 
-                maxResults: 25, 
-				type: "video",
-				videoEmbeddable: "true",
+            url: "https://www.googleapis.com/youtube/v3/search",
+            data: {
+                part: "snippet",
+                maxResults: 25,
+                type: "video",
+                videoEmbeddable: "true",
                 key: "AIzaSyDxkmLJ32YwnuN4b7vuHfxpGrbZ99edrbE",
                 q: userInput
             },
-			type: "GET",
-            dataType : "json",
-            success : callback,
-            error: function(XMLHttpRequest, textStatus, errorThrown) { 
-        alert("Status: " + textStatus); alert("Error: " + errorThrown); 
-    }   
+            type: "GET",
+            dataType: "json",
+            success: callback,
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+                alert("Status: " + textStatus);
+                alert("Error: " + errorThrown);
+            }
         };
-        
+
         $.ajax(settings);
-        
+
     };
-	
-	function displayYoutubeData(data){
-		
-		const videoUrl = `https://www.youtube.com/watch?v=${data.items[5].id.videoId}`
-		const thumbnailUrl = data.items[5].snippet.thumbnails.high.url
-		
-//		console.log(thumbnailUrl);
-		
-		$(".popup-youtube").attr("href", videoUrl);
-		$(".thumbnail").attr("src", thumbnailUrl);
-	}
-	
-    
-	
-	
-	
-/******************Photo Data*************************/	
-	
-	
-	    function getPhotoResult(userInput, callback){
-        
-   
-    	const settings = {
-            url : `https://rest.bandsintown.com/artists/${userInput}?app_id=bfbda5c7d01ac156661c3c54a7a5957f`,
-            data : {
-				part: "snippet"
+
+    function displayYoutubeData(data) {
+
+        const videoUrl = `https://www.youtube.com/watch?v=${data.items[5].id.videoId}`
+        const thumbnailUrl = data.items[5].snippet.thumbnails.high.url
+
+        //        console.log(thumbnailUrl);
+
+        $(".popup-youtube").attr("href", videoUrl);
+        $(".thumbnail").attr("src", thumbnailUrl);
+    }
+
+
+
+
+
+    /******************Photo Data*************************/
+
+
+    function getPhotoResult(userInput, callback) {
+
+
+        const settings = {
+            url: `https://rest.bandsintown.com/artists/${userInput}?app_id=bfbda5c7d01ac156661c3c54a7a5957f`,
+            data: {
+                part: "snippet"
             },
             type: "GET",
-            dataType : "json",
-            success : callback,
-            error: function(XMLHttpRequest, textStatus, errorThrown) { 
-                alert("Status: " + textStatus); alert("Error: " + errorThrown); 
-            }   
+            dataType: "json",
+            success: callback,
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+                alert("Status: " + textStatus);
+                alert("Error: " + errorThrown);
+            }
         };
-        
+
         $.ajax(settings);
-	}
-	
-		function displayPhotoData(data){
-		
-//		console.log(data.image_url);
-		
-		const imgUrl = data.image_url;
-		
-		$(".band-picture img").attr("src", imgUrl);
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-/******************Event Time & Venue Data*************************/	
-	
-	
-	function getConcertInfo(userInput, callback){
-		
-		const settings = {
-            url : `https://rest.bandsintown.com/artists/${userInput}/events?app_id=bfbda5c7d01ac156661c3c54a7a5957f`,
-            data : {
-				part: "snippet"
+    }
+
+    function displayPhotoData(data) {
+
+        //        console.log(data.image_url);
+
+        const imgUrl = data.image_url;
+
+        $(".band-picture img").attr("src", imgUrl);
+    }
+
+
+
+
+
+
+
+
+    /******************Event Time & Venue Data*************************/
+
+
+    function getConcertInfo(userInput, callback) {
+
+        const settings = {
+            url: `https://rest.bandsintown.com/artists/${userInput}/events?app_id=bfbda5c7d01ac156661c3c54a7a5957f`,
+            data: {
+                part: "snippet"
             },
             type: "GET",
-            dataType : "json",
-            success : callback,
-            error: function(XMLHttpRequest, textStatus, errorThrown) { 
-                alert("Status: " + textStatus); alert("Error: " + errorThrown); 
-            }   
+            dataType: "json",
+            success: callback,
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+                alert("Status: " + textStatus);
+                alert("Error: " + errorThrown);
+            }
         };
-        
+
         $.ajax(settings);
-		
-	}
-	
-	
 
-	
-	function displayConcertInfo(data){
-		
-		
-		
-		const eventTime = data[0].datetime;
-		const eventCity = data[0].venue.city;
-		const eventVenue = data[0].venue.name;
-		const eventRegion = data[0].venue.region;
-		
-		$(".when").text(eventTime);
-		$(".venue-name").html(`${eventCity}, ${eventRegion}<br/>${eventVenue}`);
-		
-		const map = `https://www.google.com/maps/embed/v1/place?key=AIzaSyBdNRsY4zEYnRfcQ0_ZVVd370D7yuApzhI&q=${eventVenue},${eventCity}"&maptype=roadmap`;
-		
-		console.log(map);
-		
-		$(".popup-gmaps").attr("href", map);
-	}
-	
-	
-	
-	
-	
+    }
 
-	
-	
 
-	
-	
-	
-	
-	
-	
-	
-	
-/****************POPUP ANIMATE*******************/
-	
-	
+
+
+    function displayConcertInfo(data) {
+
+
+
+        const eventTime = data[0].datetime;
+        const eventCity = data[0].venue.city;
+        const eventVenue = data[0].venue.name;
+        const eventRegion = data[0].venue.region;
+
+        $(".when").text(eventTime);
+        $(".venue-name").html(`${eventCity}, ${eventRegion}<br/>${eventVenue}`);
+
+        const map = `https://www.google.com/maps/embed/v1/place?key=AIzaSyBdNRsY4zEYnRfcQ0_ZVVd370D7yuApzhI&q=${eventVenue},${eventCity}"&maptype=roadmap`;
+
+        console.log(map);
+
+        $(".popup-gmaps").attr("href", map);
+    }
+
+
+
+
+
+
+
+
+
+    /****************POPUP ANIMATE*******************/
+
+
     $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
         disableOn: 700,
         type: 'iframe',
@@ -204,6 +196,21 @@ $(document).ready(function () {
             }
         }
     });
+
+
+    //    $(window).resize(function () { //checking for window resize event
+    //
+    //        if ($(window).width() < 414) { //checking for window width
+    //            if ($("input").is(":focus")) {
+    //                $('.content').css({
+    //                    'margin-top': -15 + 'px'
+    //                });
+    //                $('.footer').css({
+    //                    'margin-bottom': -100 + 'px'
+    //                });
+    //            }
+    //        }
+    //    });
 
 });
 
@@ -332,8 +339,5 @@ window.onload = function () {
         if (lastContent != "") return;
         targetContent = "search here...";
         refresh();
-    }, 1000);
+    }, 300);
 }
-
-
-
