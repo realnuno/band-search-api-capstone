c53750ad9a02f285c88cbd1689e4db901fc7c584
realnuno <inbeom918@gmail.com>
2018-06-20 23:52:56 -0400

    added-jquery-api

--- index.js
+++ index.js
@@ -1,3 +1,220 @@
+
+
+
+$(document).ready(function () {
+	
+	
+	
+	
+	let userSearch;
+	
+	 $("input").keydown(function (e) {
+        e.stopPropagation();
+        if (e.keyCode === 13) {
+            e.preventDefault();
+			
+			userSearch = $("input").val();
+			$("input").val("");
+			
+			getPhotoResult(userSearch, displayPhotoData);
+			
+			getYoutubeResult(userSearch, displayYoutubeData)
+			
+			getConcertInfo(userSearch, displayConcertInfo);
+			
+			
+            $('html, body').animate({
+                scrollTop: $('.js--results').offset().top
+            }, 1000);
+        };
+    });
+	
+	
+    
+    
+    
+/******************Youtube Data*************************/
+	
+	
+    function getYoutubeResult(userInput, callback){
+        
+        const settings = {
+            url : "https://www.googleapis.com/youtube/v3/search",
+            data : {
+				part: "snippet", 
+                maxResults: 25, 
+				type: "video",
+				videoEmbeddable: "true",
+                key: "AIzaSyDxkmLJ32YwnuN4b7vuHfxpGrbZ99edrbE",
+                q: userInput
+            },
+			type: "GET",
+            dataType : "json",
+            success : callback,
+            error: function(XMLHttpRequest, textStatus, errorThrown) { 
+        alert("Status: " + textStatus); alert("Error: " + errorThrown); 
+    }   
+        };
+        
+        $.ajax(settings);
+        
+    };
+	
+	function displayYoutubeData(data){
+		
+		const videoUrl = `https://www.youtube.com/watch?v=${data.items[5].id.videoId}`
+		const thumbnailUrl = data.items[5].snippet.thumbnails.high.url
+		
+//		console.log(thumbnailUrl);
+		
+		$(".popup-youtube").attr("href", videoUrl);
+		$(".thumbnail").attr("src", thumbnailUrl);
+	}
+	
+    
+	
+	
+	
+/******************Photo Data*************************/	
+	
+	
+	    function getPhotoResult(userInput, callback){
+        
+   
+    	const settings = {
+            url : `https://rest.bandsintown.com/artists/${userInput}?app_id=bfbda5c7d01ac156661c3c54a7a5957f`,
+            data : {
+				part: "snippet"
+            },
+            type: "GET",
+            dataType : "json",
+            success : callback,
+            error: function(XMLHttpRequest, textStatus, errorThrown) { 
+                alert("Status: " + textStatus); alert("Error: " + errorThrown); 
+            }   
+        };
+        
+        $.ajax(settings);
+	}
+	
+		function displayPhotoData(data){
+		
+//		console.log(data.image_url);
+		
+		const imgUrl = data.image_url;
+		
+		$(".band-picture img").attr("src", imgUrl);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+/******************Event Time & Venue Data*************************/	
+	
+	
+	function getConcertInfo(userInput, callback){
+		
+		const settings = {
+            url : `https://rest.bandsintown.com/artists/${userInput}/events?app_id=bfbda5c7d01ac156661c3c54a7a5957f`,
+            data : {
+				part: "snippet"
+            },
+            type: "GET",
+            dataType : "json",
+            success : callback,
+            error: function(XMLHttpRequest, textStatus, errorThrown) { 
+                alert("Status: " + textStatus); alert("Error: " + errorThrown); 
+            }   
+        };
+        
+        $.ajax(settings);
+		
+	}
+	
+	
+
+	
+	function displayConcertInfo(data){
+		
+		
+		
+		const eventTime = data[0].datetime;
+		const eventCity = data[0].venue.city;
+		const eventVenue = data[0].venue.name;
+		const eventRegion = data[0].venue.region;
+		
+		$(".when").text(eventTime);
+		$(".venue-name").html(`${eventCity}, ${eventRegion}<br/>${eventVenue}`);
+		
+		const map = `https://www.google.com/maps/embed/v1/place?key=AIzaSyBdNRsY4zEYnRfcQ0_ZVVd370D7yuApzhI&q=${eventVenue},${eventCity}"&maptype=roadmap`;
+		
+		console.log(map);
+		
+		$(".popup-gmaps").attr("href", map);
+	}
+	
+	
+	
+	
+	
+
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+/****************POPUP ANIMATE*******************/
+	
+	
+    $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+
+        fixedContentPos: false
+    });
+
+
+    $('.popup-gallery').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+            titleSrc: function (item) {
+                return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+            }
+        }
+    });
+
+});
+
+
+
+
+
+
+/****************INPUT ANIMATE*******************/
+
+
 var input;
 var cursor;
 var hiddenInput;
@@ -115,69 +332,8 @@ window.onload = function () {
         if (lastContent != "") return;
         targetContent = "search here...";
         refresh();
-    }, 2000);
+    }, 1000);
 }
 
 
 
-
-
-
-
-
-
-$(document).ready(function () {
-    $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
-        disableOn: 700,
-        type: 'iframe',
-        mainClass: 'mfp-fade',
-        removalDelay: 160,
-        preloader: false,
-
-        fixedContentPos: false
-    });
-
-
-    $('.popup-gallery').magnificPopup({
-        delegate: 'a',
-        type: 'image',
-        tLoading: 'Loading image #%curr%...',
-        mainClass: 'mfp-img-mobile',
-        gallery: {
-            enabled: true,
-            navigateByImgClick: true,
-            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
-        },
-        image: {
-            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-            titleSrc: function (item) {
-                return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
-            }
-        }
-    });
-
-
-
-
-
-
-
-
-
-    $("input").keydown(function (e) {
-        e.stopPropagation();
-        if (e.keyCode === 13) {
-            $("input").val("");
-            e.preventDefault();
-            console.log($("input").val());
-            $('html, body').animate({
-                scrollTop: $('.js--results').offset().top
-            }, 1000);
-        };
-    });
-
-
-
-
-
-});
